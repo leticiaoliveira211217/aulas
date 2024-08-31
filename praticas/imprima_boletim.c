f6e65798d8b9a9e4706a64b6297a74d01ca3462d
leticiaoliveira211217 <leticiaoliveira2111217@gmail.com>
2024-08-31 21:09:43 +0000

    Aula 02

---
+++ praticas/imprima_boletim.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  printf("\nMatricula	\tA1	\tA2	\tMedia");
+  printf("\n20210010	\t8.5	\t5.0	 \t6.4");   
+  printf("\n20215053	\t4.7	\t10.0 \t7.9");
+  printf("\n20220027	\t5.6	\t8.3	 \t7.2");
+  printf("\n20220103	\t9.2	\t1.5	 \t4.6");
+  
+  /*printf("\t20220027	5.6	8.3	7.2	
+  printf("\t20220103	9.2	1.5	4.6
+  return 0;*/
+}
+  
---
+++ praticas/imprima_nota_legal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+printf("\n=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t");
+printf("\nN O T A    \tL E G A L");
+printf("\n=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t");
+printf("\nProduto         \tQtd \tValor \tUnit");  
+printf("\nCamiseta        \t002         \t39.99 \tR$");
+Calca           001      89.90
+Meia Social     003      19.99
+==============================
+Total:                  229.85
